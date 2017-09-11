5279f5cd30c03ee6752b229fbcab774e33d995b1
Naggi <nagase2@gmail.com>
2017-09-11 12:22:03 +0900

    更新

---
+++ src/main/java/gittest/Hellow2.java
@@ -0,0 +1,11 @@
+package gittest;
+
+public class Hellow2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//これは必要な変更
+		System.out.println("コミット１");
+	}
+
+}
---
+++ src/main/java/gittest/Helloworld3.java
@@ -0,0 +1,10 @@
+package gittest;
+
+public class Helloworld3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("コミット３");
+	}
+
+}
